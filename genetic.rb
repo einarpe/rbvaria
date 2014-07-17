fe77ad8a64524f7d2a09563d97772d686edfda82
einarpe <kubar3k+gh@gmail.com>
2014-07-17 15:32:08 +0200

    Another little refactoring

--- genetic.rb
+++ genetic.rb
@@ -71,27 +71,25 @@ class Genetic
   # compute fitness of each chromosome in population
   # resulting array is sorted by fitness of each chromosome
   def fitness_of_population()
-    fitness = []
+    @fitness = []
     
     # apply fit function
     @population.each_index { |i|
       c = @population[i]
-      fitness << {
+      @fitness << {
         :fit => fit(c.value),
         :idx => i
       }
     }
     
     # ascending sort by distance from x-axis
-    fitness.sort! { |f1, f2|  f1[:fit] <=> f2[:fit]  }
-    
-    return fitness
+    @fitness.sort! { |f1, f2|  f1[:fit] <=> f2[:fit]  }
   end
   
   # mutate the best chromosome
-  def mutate_one(fitness_array)
+  def mutate_one()
     # get the best
-    best = fitness_array[0]
+    best = @fitness[0]
     
     # mutate it
     new_one = @population[ best[:idx] ].mutate
@@ -101,9 +99,9 @@ class Genetic
   end
   
   # remove the worst chromosome, it is so useless...
-  def remove_one(fitness_array)
+  def remove_one()
     # get the worst
-    worst = fitness_array[fitness_array.length - 1]
+    worst = @fitness[@fitness.length - 1]
     
     # remove the one
     @population.delete_at( worst[:idx] )
@@ -111,10 +109,9 @@ class Genetic
 
   # magic! ( ͡° ͜ʖ ͡°) 
   def next()
-    fitness_array = fitness_of_population
-    
-    mutate_one(fitness_array)
-    remove_one(fitness_array)
+    fitness_of_population()
+    mutate_one()
+    remove_one()
     
     @generation += 1
   end
@@ -124,7 +121,7 @@ end
 # -------------------------------------------------------- #
 
 # some sample how it works
-g = Genetic.new(24, -11.0, 11.0)
+g = Genetic.new(12, -11.0, 11.0)
 
 # -x^3 +4x^2 +3x -1
 g.func = lambda { |x| (-x ** 3) + 4 * (x ** 2) + 3.0 * x - 1 }
